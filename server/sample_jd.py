c1d9c25a5fa90357edba2140cf03e713eaab1e6c
lakshyaag <lakshya.agarwal@mail.mcgill.ca>
2024-03-29 21:50:08 -0400

    start learning testing

---
+++ server/sample_jd.py
@@ -0,0 +1,65 @@
+job_description = """
+At Desjardins, we believe in equity, diversity and inclusion. We're committed to welcoming, respecting and valuing people for who they are as individuals, learning from their differences, embracing their uniqueness, and providing a positive workplace for all. At Desjardins, we have zero tolerance for discrimination of any kind. We believe our teams should reflect the diversity of the members, clients and communities we serve.
+
+If there's something we can do to help make the recruitment process or the job you're applying for more accessible, let us know. We can provide accommodations at any stage in the recruitment process. Just ask!
+
+Job Level
+
+NV-10
+
+As a, you collect, process, analyze and use data that comes through different channels. You extract, describe, analyze and interpret the data needed to make informed business decisions.
+
+You lead stakeholders in projects for designing analytical solutions adapted to a wide range of business needs and in complex strategic data collection, processing and analysis interventions.
+
+You make recommendations on the planning and execution of operationally and conceptually complex analytical projects and initiatives that involve using your ability to use multiple databases, your analytical skills and your in depth, comprehensive understanding of the line of business and the organization.
+
+Your initiatives require extensive, in-depth knowledge of your line of work.
+
+You serve as a  and subject matter expert for various stakeholders, and as a resource person and coach for decision-making bodies. You interact with many stakeholders in other fields to understand and communicate issues, and influence decision-making.
+
+General Information on the Position
+
+Main Responsibilities
+• Analyze and identify user needs and work on complex mandates to collect, process and analyze data, taking into account the specifics of their operationalization and ensuring coordination during the rollout
+• Develop and improve reports and tools for interpreting operationally and conceptually complex data to influence the sector's strategic decision-making
+• Target opportunities to improve work methods and processes, and recommend improvements to data management tools
+• Serve as specialist advisor to help teams understand and prioritize their needs, and identify solutions in terms of collecting, processing, analyzing and interpreting data
+• Perform descriptive and diagnostic data analyses for a wide range of complex mandates and make recommendations to the relevant decision-making bodies from a data collection, processing and analysis perspective.
+
+Other Working Conditions
+• Workplace: Position located in Lévis or Montreal, depending on the applicant selected. The work arrangement for the position is hybrid work
+• Number of jobs available: 1
+
+Qualifications
+• Bachelor's degree in a related field
+• A minimum of 6 years of relevant experience
+
+Please note that other combinations of qualifications and relevant experience may be considered
+• For vacant positions available in Quebec, please note that knowledge of French is required
+
+Desjardins Cross-sector Skills
+
+Action oriented, Customer Focus, Differences, Nimble learning
+
+Key competencies for the job
+
+Ambiguity, Business insight, Communicates effectively, Complexity, Decision quality, Interpersonal Savvy, Strategic mindset
+
+Work Location
+
+1, Complexe Desjardins, Montréal
+
+Trade Union
+
+Non Syndiqué
+
+Unposting Date
+
+2024-04-4
+
+Job Family
+
+Data (FG)
+
+Desjardins Group is the largest cooperative financial group in Canada, and one of the largest employers in the country. It offers a full range of financial products and services and is home to a wealth of expertise in property and casualty insurance, life and health insurance, wealth management, services for businesses of all sizes, securities brokerage, asset management, venture capital, and secure, leading-edge virtual access methods
+"""
