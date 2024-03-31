1cceef2e9b5103f2f62f436226a10b6e327a09dc
lakshyaag <lakshya.agarwal@mail.mcgill.ca>
2024-03-31 02:35:38 -0400

    chore: cleanup

--- server/models_new.py
+++ server/models_new.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List, Optional
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field, HttpUrl
+from pydantic import BaseModel, EmailStr, Field, HttpUrl
 
 
 class AdditionalRequirement(BaseModel):
