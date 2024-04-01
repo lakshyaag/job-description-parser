2a7b2f472c5ad81f6474e43dca9f7380b4f56c3b
lakshyaag <lakshya.agarwal@mail.mcgill.ca>
2024-03-31 23:44:13 -0400

    fix: recommendation view

--- client/src/components/ResultSections/ResumeRecommendation.tsx
+++ client/src/components/ResultSections/ResumeRecommendation.tsx
@@ -33,15 +33,19 @@ const ResumeRecommendationSection: React.FC<ResumeRecommendationProps> = ({
   recommendations,
   theme,
 }) => {
+  const filteredRecommendations = recommendations.recommendations.filter(
+    (recommendation) => recommendation.updated_sentence
+  );
+
   return (
-    <Accordion type="single">
+    <Accordion type="single" defaultValue="resume_recommendations">
       <AccordionItem value="resume_recommendations">
         <AccordionTrigger className="font-bold text-md text-gray-800 dark:text-gray-200">
           Resume Recommendations
         </AccordionTrigger>
         <AccordionContent>
           <Accordion type="single">
-            {recommendations.recommendations.map((recommendation, index) => (
+            {filteredRecommendations.map((recommendation, index) => (
               <AccordionItem key={index} value={`recommendation_${index}`}>
                 <AccordionTrigger className="font-bold text-sm text-gray-800 dark:text-gray-200">
                   Recommendation {index + 1}
