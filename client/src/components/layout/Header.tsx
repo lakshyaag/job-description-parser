3911f9d1f1cee6bb7b8fb4f98b6d33df38ec74e2
lakshyaag <lakshya.agarwal@mail.mcgill.ca>
2024-04-01 20:05:40 -0400

    fix: add mobile nav

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -5,28 +5,30 @@ import Github from "../icons/GitHub";
 import { ToggleTheme } from "../icons/ThemeToggle";
 import {
   SignInButton,
-  SignOutButton,
   SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 const Header = () => {
   return (
-    <header className="bg-base-100/80 sticky top-0 z-10 mx-2 my-2 px-16 py-2 backdrop-blur-lg border-b border-gray-200">
-      <section className="mx-auto gap-4 flex flex-col items-center justify-between py-2 md:flex-row">
+    <header className="bg-base-100/80 sticky top-0 z-10 px-4 py-3 mx-2 backdrop-blur-lg border-b border-gray-200">
+      <section className="mx-auto flex items-center justify-between">
         <div className="flex items-center">
           <Link href="/">
             <div className="flex flex-row items-center">
-              <span className="text-2xl font-bold">
+              <span className="text-xl font-bold md:text-2xl">
                 {metadata.title as React.ReactNode}
               </span>
             </div>
           </Link>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="hidden md:flex items-center space-x-4">
+          {/* Desktop menu items */}
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100"
             href="https://github.com/lakshyaag/"
@@ -48,6 +50,40 @@ const Header = () => {
               <Button variant={"outline"}>Sign up</Button>
             </SignUpButton>
           </SignedOut>
+        </div>
+        <div className="md:hidden">
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="ghost" size="sm">
+                <HamburgerMenuIcon className="h-4 w-4" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-4">
+              <div className="flex flex-col space-y-4 items-center">
+                {/* Mobile menu items */}
+                <a
+                  className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100"
+                  href="https://github.com/lakshyaag/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Github />
+                </a>
+                <ToggleTheme />
+                <SignedIn>
+                  <UserButton afterSignOutUrl={"/"} />
+                </SignedIn>
+                <SignedOut>
+                  <SignInButton mode={"modal"}>
+                    <Button>Sign in</Button>
+                  </SignInButton>
+                  <SignUpButton mode={"modal"}>
+                    <Button variant={"outline"}>Sign up</Button>
+                  </SignUpButton>
+                </SignedOut>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </section>
     </header>
